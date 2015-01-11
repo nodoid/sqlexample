1bb9f321596389ec28fb831a86111c6fbea7b446
nodoid <paul@snuberry-software.co.uk>
2015-01-11 02:41:19 +0000

    updated

---
+++ Android/sqlexample/sqlexample/DataModels/Videos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+namespace sqlexample
+{
+    public class Videos : IIdentity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; private set; }
+
+        public string videoname { get; set; }
+
+        public int humanid { get; set; }
+
+        public DateTime recordedon { get; set; }
+    }
+}
+
